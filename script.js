7206535442a96a0fcddbc9dd43280fe49d9bb241
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-02 16:41:26 +0100

    esercizio FizzBuzz

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 for (let i = 1; i <= 100; i++) {
     
-    if (i % 3 === 0)
+    if (i % 3 === 0 && i % 5 === 0)
+    {
+        console.log("FizzBuzz");  
+    }
+
+    else if (i % 3 === 0)
     {
         console.log("Fizz");
     }
@@ -14,12 +19,7 @@ for (let i = 1; i <= 100; i++) {
     {
         console.log("Buzz");
     }
-
-    if (i % 3 === 0 )
-    {
-        console.log("FizzBuzz");
-    }
-
+    
     else 
     {
         console.log(i);
