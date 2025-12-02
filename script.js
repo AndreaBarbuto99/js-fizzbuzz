0b314cadd9f54fe014163ef4a7e86713038bd9b9
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-02 17:00:13 +0100

    feat: aggiunta stampa su html

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 /*Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
-
+let htmlstamp = document.getElementById("lista");
 
 for (let i = 1; i <= 100; i++) {
     
     if (i % 3 === 0 && i % 5 === 0)
     {
-        console.log("FizzBuzz");  
+        htmlstamp.innerHTML += `<li>FizzBuzz</li>`;  
     }
 
     else if (i % 3 === 0)
     {
-        console.log("Fizz");
+        htmlstamp.innerHTML += `<li>Fizz</li>`;
     }
 
     else if (i % 5 === 0)
     {
-        console.log("Buzz");
+        htmlstamp.innerHTML += `<li>Buzz</li>`;
     }
     
     else 
     {
-        console.log(i);
+        htmlstamp.innerHTML += `<li>${i}</li>`;
     }
 }
